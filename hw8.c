2c2122395d5217ed66f11a09dd60b494fc51a972
Yu-Cheng Liang <a135246a135246@gmail.com>
2016-01-10 12:46:37 +0800

    code clean up, more comments

--- hw8.c
+++ hw8.c
@@ -12,21 +12,17 @@ typedef struct ip IP;
 typedef unsigned int ui;
 
 //prototype
-inline unsigned long long int rdtsc();
-ui bingen(ui[]);
-ui sigbit(ui, ui);
-IP* insert_node(IP*, ui, ui);
-IP* LPM_search(IP*, ui);
-void IPdecode(IP*, FILE*);
-IP* delete_node(IP*, ui, ui);
-void free_list(IP*);
-void print_list(IP*, FILE*);
-
-//global variables
-unsigned long long int begin, end;
+ui bingen(ui[]); //convert IPv4 address into 32-bit unsigned int
+ui sigbit(ui, ui); //return which element in array should the ip be stored
+IP* insert_node(IP*, ui, ui); //insert a node
+IP* LPM_search(IP*, ui); //search data, will return matched node address or NULL
+void IPdecode(IP*, FILE*); //decode 32-bit unsigned int into IPv4 address
+IP* delete_node(IP*, ui, ui); //delete node
+void free_list(IP*); //free linked list after use
+//void print_list(IP*, FILE*); //data structure checker
 
 int main() {
-	FILE *origin, *search, *insert, *delete, *result, *timing;
+	FILE *origin, *search, *insert, *delete, *result/*, *timing*/;
 	ui pow_two = 1, ip[5], ipbin = 0, bin_12 = 0, bin_8 = 0, sig = 0;
 	int i;
 	char temp[21];
@@ -38,12 +34,12 @@ int main() {
 	insert = fopen("IPv4_insert.txt", "r");
 	delete = fopen("IPv4_delete.txt", "r");
 	result = fopen("result.txt", "w");
-	timing = fopen("timing.csv", "w");
+//	timing = fopen("timing.csv", "w");
 
 //make segmentation tables
-	IP *seg_table1[pow_two << 8];
-	IP *seg_table2[pow_two << 12];
-	IP *seg_table3[pow_two << 12];
+	IP *seg_table1[pow_two << 8];  //length 8-15
+	IP *seg_table2[pow_two << 12]; //length 16-25
+	IP *seg_table3[pow_two << 12]; //length 25-32
 	
 	for(i = 0;i < pow_two << 8;i++) {
 		seg_table1[i] = NULL;
@@ -68,7 +64,7 @@ int main() {
 			exit(1);
 		}
 	}
-
+/* used for check data structure
 	for(i = 0; i < 1 << 8;i++) {
 		print_list(seg_table1[i], timing);
 		fprintf(timing, "==============================\n");
@@ -78,14 +74,15 @@ int main() {
 		fprintf(timing, "==============================\n");
 		print_list(seg_table3[i], timing);
 	}
-//	fprintf(timing, "search");
+*/
+
 	while(fgets(temp, 21, search) != NULL) { //search
-		begin = rdtsc();
 		sscanf(temp, "%u.%u.%u.%u", &ip[0], &ip[1], &ip[2], &ip[3]);
 		ipbin = bingen(ip);
 		bin_12 = sigbit(ipbin, 24);
 		bin_8 = sigbit(ipbin, 8);
-		
+
+//serach from 25-32 lengthed group, if no match result, go search next
 		matched = LPM_search(seg_table3[bin_12], ipbin);
 		if(matched == NULL)
 			matched = LPM_search(seg_table2[bin_12], ipbin);
@@ -93,14 +90,9 @@ int main() {
 			matched = LPM_search(seg_table1[bin_8], ipbin);
 		
 		IPdecode(matched, result);
-		end = rdtsc();
-//		fprintf(timing, "%lld\n", end - begin);
 	}
-//	fprintf(timing, "\n");
 
-//	fprintf(timing, "insert");
 	while(fgets(temp , 21, insert) != NULL) { //insert
-		begin = rdtsc();
 		sscanf(temp, "%u.%u.%u.%u/%u", &ip[0], &ip[1], &ip[2], &ip[3], &ip[4]);
 		ipbin = bingen(ip);
 		sig = sigbit(ipbin, ip[4]);
@@ -112,31 +104,23 @@ int main() {
 			seg_table3[sig] = insert_node(seg_table3[sig], ipbin, ip[4]);
 		else if(ip[4] != 0 && ipbin != 0) {
 			printf("Error in input data.");
-			exit(1);
 		}
-		end = rdtsc();
-//		fprintf(timing, "%lld\n", end - begin);
 	}
-//	fprintf(timing, "\n");
 
-//	fprintf(timing, "delete");
 	while(fgets(temp, 21, delete) != NULL) { //delete
-		begin = rdtsc();
 		sscanf(temp, "%u.%u.%u.%u/%u", &ip[0], &ip[1], &ip[2], &ip[3], &ip[4]);
 		ipbin = bingen(ip);
 		sig = sigbit(ipbin, ip[4]);
 		if(ip[4] >= 8 && ip[4] < 16)
 			seg_table1[sig] = delete_node(seg_table1[sig], ipbin, ip[4]);
 		else if(ip[4] >= 16 && ip[4] < 25)
-			delete_node(seg_table2[sig], ipbin, ip[4]);
+			seg_table2[sig] = delete_node(seg_table2[sig], ipbin, ip[4]);
 		else if(ip[4] >= 25 && ip[4] <= 32)
-			delete_node(seg_table3[sig], ipbin, ip[4]);
+			seg_table3[sig] = delete_node(seg_table3[sig], ipbin, ip[4]);
 		else if(ip[4] != 0 && ipbin != 0) {
 			printf("Error in input data.");
 			exit(1);
 		}
-		end = rdtsc();
-//		fprintf(timing, "%lld\n", end - begin);
 	}
 	
 //free total list
@@ -153,19 +137,13 @@ int main() {
 	fclose(search);
 	fclose(delete);
 	fclose(result);
-	fclose(timing);
+//	fclose(timing);
 	return 0;
-}
-	
-inline unsigned long long int rdtsc() {
-	unsigned long long int x;
-	asm volatile ("rdtsc" : "=A" (x));
-	return x;
 }
 
 ui bingen(ui ip[]) {
-	ui out = 0x00000000;
-	out |= (ip[0] << 24);
+	ui out = 0x00000000; 
+	out |= (ip[0] << 24); 
 	out |= (ip[1] << 16);
 	out |= (ip[2] << 8);
 	out |= ip[3];
@@ -237,7 +215,7 @@ void IPdecode(IP *match, FILE *result) {
 		length = match -> length;
 		fprintf(result, "%u.%u.%u.%u/%u\n", ip[0], ip[1], ip[2], ip[3], length);
 	}
-	else {
+	else { //match == NULL means there are no matched prefix, use default
 		fprintf(result, "%s", "0.0.0.0/0\n");
 	}
 	return;
@@ -275,7 +253,7 @@ void free_list(IP *head) {
 		to_free = head;
 	}
 }
-
+/* used for check data structure
 void print_list(IP *head, FILE *timing) {
 	IP *temp = head;
 	while(temp != NULL) {
@@ -284,3 +262,4 @@ void print_list(IP *head, FILE *timing) {
 	}
 	return;
 }
+*/
